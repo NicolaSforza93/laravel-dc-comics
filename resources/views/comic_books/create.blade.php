f3efd95de10b41a76f381625e987a11026759592
NicolaSforza93 <nicola.sforza93@gmail.com>
2023-12-22 21:10:35 +0100

    ADD - fix create.blade.php style

--- resources/views/comic_books/create.blade.php
+++ resources/views/comic_books/create.blade.php
@@ -2,54 +2,59 @@
 
 @section('content')
 
-<section class="section-create p-4">
+<section class="section-create py-4">
     <div class="container">
-        <form action="{{ route('comic_books.store') }}" method="POST" class="p-3 w-50 m-auto">
-            @csrf
-            <div class="mb-3">
-                <label class="form-label fw-bold text-uppercase bg-white p-1 rounded">Title</label>
-                <input type="text" class="form-control border border-3 border-dark" name="title" id="title" value="{{ old('title') }}">
-            </div>
+        <figure class="position-relative mb-0">
 
-            <div class="mb-3">
-                <label class="form-label fw-bold text-uppercase bg-white p-1 rounded">description</label>
-                <textarea class="form-control border border-3 border-dark" name="description" aria-label="With textarea">{{ old('description') }}</textarea>
-            </div>
-
-            <div class="mb-3">
-                <label for="basic-url" class="form-label fw-bold text-uppercase bg-white p-1 rounded">URL Image</label>
-                <input type="text" class="form-control border border-3 border-dark" name="thumb" id="basic-url" aria-describedby="basic-addon3 basic-addon4" value="{{ old('thumb') }}">
-            </div>
-
-            <div class="mb-3">
-                <label for="basic-url" class="form-label fw-bold text-uppercase bg-white p-1 rounded">Price</label>
-                <div class="input-group">
-                    <span class="input-group-text border border-3 border-dark">$</span>
-                    <input type="text" class="form-control border border-3 border-dark border-start-0" name="price" aria-label="Amount (to the nearest dollar)" value="{{ old('price') }}">
+            <img src="{{ Vite::asset('resources/img/16884_1.jpg') }}" alt="" class="overlay img-fluid">
+        
+            <form action="{{ route('comic_books.store') }}" method="POST" class="w-50 position-absolute top-50 start-50 translate-middle">
+                @csrf
+                <div class="mb-3">
+                    <label class="form-label text-uppercase text-white">Title</label>
+                    <input type="text" class="form-control border border-0" name="title" id="title" value="{{ old('title') }}">
                 </div>
-            </div>
-              
-            <div class="mb-3">
-                <label for="basic-url" class="form-label fw-bold text-uppercase bg-white p-1 rounded">Series</label>
-                <input type="text" class="form-control border border-3 border-dark" name="series" aria-label="Recipient's username" aria-describedby="basic-addon2" value="{{ old('series') }}">
-            </div>
 
-            <div class="mb-3">
-                <label for="basic-url" class="form-label fw-bold text-uppercase bg-white p-1 rounded">Sale Date</label>
-                <input type="date" class="form-control border border-3 border-dark" placeholder="Data" name="sale_date" aria-label="Recipient's username" aria-describedby="basic-addon2" value="{{ old('sale_date') }}">
-            </div>
+                <div class="mb-3">
+                    <label class="form-label text-uppercase text-white">description</label>
+                    <textarea class="form-control border border-0" name="description" aria-label="With textarea">{{ old('description') }}</textarea>
+                </div>
 
-            <div class="mb-3">
-                <select class="form-select border border-3 border-dark" name="type" aria-label="Default select example">
-                    <option>Tipologia</option>
-                    <option @selected(old('type') === 'comic book') value="comic book">Comic Book</option>
-                    <option @selected(old('type') === 'graphic novel') value="graphic novel">Graphic Novel</option>
-                  </select>
-            </div> 
-            
-            <button type="submit" class="btn btn-primary">Add</button>
-              
-        </form>
+                <div class="mb-3">
+                    <label for="basic-url" class="form-label text-uppercase text-white">URL Image</label>
+                    <input type="text" class="form-control border border-0" name="thumb" id="basic-url" aria-describedby="basic-addon3 basic-addon4" value="{{ old('thumb') }}">
+                </div>
+
+                <div class="mb-3">
+                    <label for="basic-url" class="form-label text-uppercase text-white">Price</label>
+                    <div class="input-group">
+                        <span class="input-group-text border-end">$</span>
+                        <input type="text" class="form-control border-start" name="price" aria-label="Amount (to the nearest dollar)" value="{{ old('price') }}">
+                    </div>
+                </div>
+                
+                <div class="mb-3">
+                    <label for="basic-url" class="form-label text-uppercase text-white">Series</label>
+                    <input type="text" class="form-control border border-0" name="series" aria-label="Recipient's username" aria-describedby="basic-addon2" value="{{ old('series') }}">
+                </div>
+
+                <div class="mb-3">
+                    <label for="basic-url" class="form-label text-uppercase text-white">Sale Date</label>
+                    <input type="date" class="form-control border border-0" placeholder="Data" name="sale_date" aria-label="Recipient's username" aria-describedby="basic-addon2" value="{{ old('sale_date') }}">
+                </div>
+
+                <div class="mb-3">
+                    <select class="form-select border border-3 border-dark" name="type" aria-label="Default select example">
+                        <option>Tipologia</option>
+                        <option @selected(old('type') === 'comic book') value="comic book">Comic Book</option>
+                        <option @selected(old('type') === 'graphic novel') value="graphic novel">Graphic Novel</option>
+                    </select>
+                </div> 
+                
+                <button type="submit" class="btn btn-primary">Add</button>
+                
+            </form>
+        </figure>
 
         @if ($errors->any())
             <div class="alert alert-danger">
--- resources/views/comic_books/index.blade.php
+++ resources/views/comic_books/index.blade.php
@@ -9,19 +9,34 @@
             <div class="row row-gap-4">
                 @forelse ($comic_books as $comic_book)
                     <div class="col-2">
-                        <div class="card h-100 position-relative"> 
-                            <div class="delete position-absolute">
-                                <form action="{{ route('comic_books.destroy', $comic_book->id) }}" method="POST">
-        
-                                    @csrf
-                                
-                                    @method('DELETE')
-            
-                                    <button type="submit" onclick="return confirm('Sicuro di voler cancellare')">
-                                        <i class="fa-solid fa-circle-minus"></i>
-                                    </button>
-                                </form>
-                            </div>                   
+                        <div class="card h-100 position-relative">
+                            <div class="modal fade" id="modal-{{$comic_book->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-sm modal-dialog-centered">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Attenzione</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body text-center">
+                                            <h6>Vuoi davvero eliminare {{$comic_book->title}}?</h6>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-primary btn-sm" data-bs-dismiss="modal">Annulla</button>
+                                            <form action="{{route('comic_books.destroy', $comic_book->id)}}" method="POST">
+
+                                                @csrf
+
+                                                @method('DELETE')
+
+                                                <button type="submit" class="btn btn-danger btn-sm">Elimina</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div> 
+                            <button type="button" class="btn-delete position-absolute" data-bs-toggle="modal" data-bs-target="#modal-{{$comic_book->id}}">
+                                <i class="fa-solid fa-circle-minus"></i>
+                            </button>                  
                             <img src="{{ $comic_book->thumb }}" class="object-fit-contain rounded" alt="">
                             <div class="card-body px-0">
                                 <h6 class="text-uppercase text-black">
@@ -34,7 +49,7 @@
                     <p>Nessun fumetto trovato</p>
                 @endforelse
             </div>
-            <button type="button" class="add-btn mb-3 align-self-center"><a href="{{ route('comic_books.create') }}" class="text-white text-decoration-none text-uppercase">Add Comic Book <i class="fa-solid fa-plus"></i></a></button>
+            <button type="button" class="add-btn mb-3 align-self-center"><a href="{{ route('comic_books.create') }}" class="text-white text-decoration-none text-uppercase">Add Comic Book</a></button>
         </div>
     </div>
 </section>
